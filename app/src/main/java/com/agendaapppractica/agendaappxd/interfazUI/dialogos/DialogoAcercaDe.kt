f1ce681c63004aca57891821cab4eeebaf16cf80
christoperlol <chnovoa@cftla.cl>
2026-07-01 07:04:06 -0400

    Fix Bugs interface features and screens Insert Little Tutorial in every screen

--- app/src/main/java/com/agendaapppractica/agendaappxd/interfazUI/dialogos/DialogoAcercaDe.kt
+++ app/src/main/java/com/agendaapppractica/agendaappxd/interfazUI/dialogos/DialogoAcercaDe.kt
@@ -1,3 +1,5 @@
+package com.agendaapppractica.agendaappxd.interfazUI.dialogos
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
